7af947c1199b3495c52511a70eb4e3b2eec4d798
jbpraxxys <jbbernardo.praxxys@gmail.com>
2026-07-19 00:42:04 +0800

    pos: create Charge button component with total display

---
+++ resources/ts/pages/dashboard/pos/cart-buttons/ns-pos-charge-button.vue
@@ -0,0 +1,41 @@
+<template>
+    <div @click="payOrder()" id="charge-button" class="flex-shrink-0 flex items-center font-bold cursor-pointer justify-center flex-auto rounded-lg bg-emerald-600 text-white text-lg lg:text-xl h-14 mx-1">
+        <i class="mr-2 text-xl las la-cash-register"></i> 
+        <span class="text-base lg:text-lg">{{ __( 'Charge' ) }} {{ nsCurrency( order.total ) }}</span>
+    </div>
+</template>
+<script lang="ts">
+declare const POS;
+declare const nsShortcuts;
+declare const nsHotPress;
+declare const __;
+
+import { nsCurrency } from '~/filters/currency';
+
+export default {
+    props: [ 'order' ],
+    methods: {
+        __,
+        nsCurrency,
+        async payOrder() {
+            POS.runPaymentQueue();
+        },
+    },
+    mounted() {
+         for( let shortcut in nsShortcuts ) {
+            if ([ 'ns_pos_keyboard_payment' ].includes( shortcut ) ) {
+                nsHotPress
+                    .create( 'ns_pos_keyboard_payment' )
+                    .whenNotVisible([ '.is-popup' ])
+                    .whenPressed( nsShortcuts[ shortcut ] !== null ? nsShortcuts[ shortcut ].join( '+' ) : null, ( event ) => {
+                        event.preventDefault();
+                        this.payOrder();
+                });
+            }
+        }
+    },
+    unmounted() {
+        nsHotPress.destroy( 'ns_pos_keyboard_payment' );
+    }
+}
+</script>
