9d77b58e60ec660ec1ab8802c889684264a5573e
jbpraxxys <jbbernardo.praxxys@gmail.com>
2026-08-18 18:13:57 +0800

    feat: enforce enrolled device on attendance clock routes

--- routes/api/attendance.php
+++ routes/api/attendance.php
@@ -1,20 +1,21 @@
 <?php
 
 use App\Http\Controllers\Dashboard\AttendanceController;
+use App\Http\Middleware\EnsureEnrolledDevice;
 use App\Http\Middleware\NsRestrictMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post( 'attendance/clock-in', [ AttendanceController::class, 'clockIn' ] )
-    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ) );
+    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ), EnsureEnrolledDevice::class );
 
 Route::post( 'attendance/clock-out', [ AttendanceController::class, 'clockOut' ] )
-    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ) );
+    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ), EnsureEnrolledDevice::class );
 
 Route::post( 'attendance/break-in', [ AttendanceController::class, 'breakIn' ] )
-    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ) );
+    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ), EnsureEnrolledDevice::class );
 
 Route::post( 'attendance/break-out', [ AttendanceController::class, 'breakOut' ] )
-    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ) );
+    ->middleware( NsRestrictMiddleware::arguments( 'attendance.clock' ), EnsureEnrolledDevice::class );
 
 Route::middleware( NsRestrictMiddleware::arguments( 'attendance.read' ) )->group( function () {
     Route::get( 'attendance/current-status', [ AttendanceController::class, 'getCurrentStatus' ] );
